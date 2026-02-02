01f9ff5a55a24ace40f74e71ed102d9a0f272163
ベル <2410041@i-seifu.jp>
2026-02-03 02:23:16 +0900

    コメント追加

--- ebamap/frontend/src/pages/StoreInfoDetail/StoreInfoDetailPage.tsx
+++ ebamap/frontend/src/pages/StoreInfoDetail/StoreInfoDetailPage.tsx
@@ -25,7 +25,7 @@ const StoreInfoDetailPage = () => {
                     <h2 className="section-title">基本情報</h2>
                     <div className="info-item">
                         <span className="info-label">店舗名</span>
-                        <span className="info-value">{currentStore.name}</span>
+                        <span className="info-value">スーパー{currentStore.name}</span>
                     </div>
                     <div className="info-item">
                         <span className="info-label">営業時間</span>
@@ -54,6 +54,8 @@ const StoreInfoDetailPage = () => {
                         <span className="info-label">最寄り駅</span>
                         <span className="info-value">{currentStore.nearestStation}</span>
                     </div>
+                    {/* {currentStore.変数} にデータが入っていたら表示 */}
+                    {/* そうでなければ非表示 */}
                     {currentStore.busStop && (
                         <div className="info-item">
                             <span className="info-label">バス停名</span>

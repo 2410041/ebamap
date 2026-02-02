cd3f168b090c45bd8e9f7a277b6a17be2f86efd1
ベル <2410041@i-seifu.jp>
2026-02-03 02:10:21 +0900

    定義を追加

--- ebamap/frontend/src/types/Store.ts
+++ ebamap/frontend/src/types/Store.ts
@@ -3,11 +3,19 @@
 // TypeScript に int 型はないため、string 型で統一
 // 電話番号は 0 や - で始まるため number 型ではなく string 型で扱う
 export interface Store {
-  id: string;
-  name: string;
-  openTime: string;
-  closeTime: string;
-  tell: string;
-  address?: string;
-  nearestStation?: string;
+    id: string;
+    name: string;
+    openTime: string;
+    closeTime: string;
+    tell: string;
+    postCode: string;
+    address?: string;
+    // 最寄り駅
+    nearestStation?: string;
+    // バス停名
+    busStop?: string;
+    // バスの行き先
+    busDestination?: string;
+    // バスや徒歩での所要時間（分）
+    busTravelTimeMinutes?: string;
 }
